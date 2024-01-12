8f23adfa5d4da8216870816ec7ea5dd178ea3962
Leslie Yip <leslieyip02@gmail.com>
2024-01-12 20:37:47 +0800

    Added tests to verify sheet formats

---
+++ test/sheet_loading_test.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'package:chords/models/sheet.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  test('Load all sheets', () {
+    rootBundle.loadString('AssetManifest.json').then((data) {
+      Map<String, dynamic> pathMap = jsonDecode(data);
+      List<String> paths = pathMap.keys
+          .where((path) => path.startsWith('assets/sheets/'))
+          .toList();
+      for (final path in paths) {
+        rootBundle.loadString(path).then((sheetValue) {
+          Sheet path = Sheet.fromString(sheetValue);
+          assert(path.title != null);
+          assert(path.composer != null);
+          assert(path.sections.isNotEmpty);
+        });
+      }
+    });
+  });
+}
